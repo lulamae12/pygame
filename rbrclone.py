31999d21a7e00a8d16beb51e1fa463156517880a
Tommy Smith <tls.tommy11@gmail.com>
2018-04-09 08:43:07 -0500

    made it to loop

--- rbrclone.py
+++ rbrclone.py
@@ -1,11 +1,13 @@
 
-import math, sys
+import math, sys, random
 import pygame as pg
 from colors import *
-
+ReadyForRepeat = False
 #setup
 steps = 5
-
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
+BALL_SIZE = 71
 pg.init()
 pg.display.set_caption("centipede")
 screen = pg.display.set_mode((600,600))
@@ -13,7 +15,7 @@ pg.mouse.set_visible(False)
 pg.display.update()
 clock = pg.time.Clock()
 gameRunning = True
-
+clock.tick(60)
 
 class Ship(object):
     def __init__(self, pos):
@@ -23,102 +25,110 @@ class Ship(object):
         self.imageRect = self.image.get_rect()
         screen.blit(self.image,self.imageRect)
         pg.display.update()
-        self.x = 100
-        self.y = 100
 
-    def getMousePos(self):
-        (self.MouseX, self.MouseY) = pg.mouse.get_pos()
-        print(self.MouseX, self.MouseY)
-    def goToMouse(self):
-        self.x = self.MouseX
-        self.y = self.MouseY
 
+        self.x = 300
+        self.y = 465
+    def getShipX(self):
+        self.shipX = self.x
+        print("x:",self.x)
+    def controls(self):
+        dist = 3
+        key = pg.key.get_pressed()
+        if key[pg.K_d]: # d key
+            self.x += dist # move right
+
+        elif key[pg.K_a]: # a key
+            self.x -= dist # move left
+        if key[pg.K_ESCAPE]:
+            sys.exit()
         #right side of screen
         if self.x > 585:
             self.x = 585
-        #bottom of screen
-        if self.y > 587:
-            self.y = 587
-
-
-    def controls(self):
-
-        key = pg.key.get_pressed()
-
-        if key[pg.K_ESCAPE]:
-            sys.exit()
-
+        #left of screen
+        if self.x < 0:
+            self.x = 0
     def update(self, surface):
         surface.blit(self.image, (self.x, self.y))
     def keepinbounds(self):
         if self.x < 3:
             self.x == 3
     def collisionCheck(self, asteroid):
-        col = asteroid.imageRect.collidepoint(pg.mouse.get_pos())
+        col = asteroid.imageRect.collidepoint(self.x, self.y)
         if col == True:
             print("hit")
-
+            sys.exit()
 class Asteroid(object):
     def __init__(self, pos):
-        self.x = 100
-        self.y = 100
-        self.image = pg.image.load("Asteroid.png")
 
+
+        screen = pg.display.get_surface()
+        screen = pg.display.get_surface()
+        self.area = screen.get_rect()
+        self.x = ship.x
+        self.y = 0
+        self.image = pg.image.load("Asteroid.png")
         self.image = pg.transform.scale(self.image, (66, 71))
         self.imageRect = self.image.get_rect()
+        self.imageRect = self.imageRect.inflate(-10,-10)
+        self.imageRect = self.imageRect.move(ship.x,0)
         screen.blit(self.image,self.imageRect)
         pg.display.update()
         self.outOfBoundsLEFT = False
         self.outOfBoundsRIGHT = False
         self.outOfBoundsTOP = False
         self.outOfBoundsBOTTOM = False
-    def keepinbounds(self):
-        #left
-        if self.x < 3:
-            self.x = 3
-            self.outOfBoundsLEFT = True
-        elif self.x > 3:
-            self.outOfBoundsLEFT = False
-        #right
-        if self.x > 583:
-            self.x = 583
-            self.outOfBoundsRIGHT = True
-        elif self.x < 583:
-            self.outOfBoundsRIGHT = False
-        #bottom
-        if self.y > 486 :
-            self.y = 486
-            self.outOfBoundsBOTTOM = True
-        elif self.y < 486:
-            self.outOfBoundsBOTTOM = False
-        #top
-        if self.y < 0:
-            self.y = 0
-            self.outOfBoundsTOP = True
-        elif self.y > 0:
-            self.outOfBoundsTOP = False
+
+
+
+    def motion(self, screen):
+
+        if self.area.contains(self.imageRect):
+            screen.blit(self.image, self.imageRect)
+            self.imageRect = self.imageRect.move(0,5)
+            screen.blit(self.image, self.imageRect)
+            pg.display.update()
+            print(self.imageRect)
+
+            print("fasle")
+        else:
+            print("true")
+
     def update(self, surface):
         surface.blit(self.image, self.imageRect)
 
+
+
+
+
+def main():
+    while gameRunning: #main loop
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                sys.exit()
+                gameRunning == False
+        ship.collisionCheck(asteroid1)
+
+
+    
+        ship.getShipX()
+        ship.controls() # handle the keys
+        screen.fill(BLACK)
+        asteroid1.update(screen)
+        ship.update(screen)
+        pg.display.update()
+        ship.keepinbounds
+        asteroid1.motion(screen)
+        """keeps ship on screen"""
+        if asteroid1.area.contains(asteroid1.imageRect):
+            ReadyForRepeat = False
+        else:
+            ReadyForRepeat = True
+        if ReadyForRepeat == True:
+            asteroid1.__init__(object)
+            asteroid1.update(screen)
+            pg.display.update()
+            asteroid1.motion(screen)
 ship = Ship(object)
 asteroid1 = Asteroid(object)
-
-
-"""main loop"""
-while gameRunning: #main loop
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            sys.exit()
-            gameRunning == False
-        ship.collisionCheck(asteroid1)
-    ship.getMousePos()
-    ship.goToMouse()
-    ship.controls() # handle the keys
-    screen.fill(BLACK)
-    asteroid1.update(screen)
-    ship.update(screen) # draw the bird to the screen
-    pg.display.update()
-    ship.keepinbounds
-
-    """keeps ship on screen"""
-    #left
+main()
